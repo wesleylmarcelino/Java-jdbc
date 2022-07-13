033247e9fa1856628b695110cedf0fa59c2400f8
wesleylmarcelino <72048448+wesleylmarcelino@users.noreply.github.com>
2022-07-13 17:35:54 -0300

    TestaConexao main

---
+++ src/principal/TestaConexao.java
@@ -0,0 +1,21 @@
+package principal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+
+	public static void main(String[] args) throws SQLException {
+		
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost"
+				+ "/loja_virtual?useTimezone=true&serverTimezone=UTC", 
+				"root", "995612");
+		
+		System.out.println("Conectou!!");
+		
+		connection.close();
+		
+		System.out.println("Conexão fechada!");
+	}
+}
